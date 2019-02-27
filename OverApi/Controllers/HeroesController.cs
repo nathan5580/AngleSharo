22159a14f85f0ad8ec79085390bd111cea0762f9
nathan5580 <nathan5580@gmail.com>
2019-02-27 17:52:42 +0100

    heroes

--- OverApi/Controllers/HeroesController.cs
+++ OverApi/Controllers/HeroesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AngleSharp;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +12,23 @@ namespace OverApi.Controllers
     [ApiController]
     public class HeroesController : ControllerBase
     {
+        [HttpGet,Route("Get")]
+        public async Task<ActionResult<IEnumerable<string>>> GetAllHeroesAsync()
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            var address = "https://playoverwatch.com/fr-fr/heroes/";
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(address);
+
+
+            using (document)
+            {
+                var cellSelector = "span.portrait-title";
+                var cells = document.QuerySelectorAll(cellSelector);
+                var titles = cells.Select(m => m.TextContent);
+
+                return titles.ToList();
+            }
+        }
     }
 }
